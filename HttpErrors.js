b4c361f2087c77d24b90abbd60f37673f7524ee3
Todd Langford-Archer <toddpla@gmail.com>
2019-04-17 23:47:37 +0100

    Add default 400 Http errors
    This errors are harded coded following the standard Http error coding
    and naming convention.

---
+++ HttpErrors.js
@@ -0,0 +1,70 @@
+const BaseError = require('./BaseError');
+const generator = require('./generator');
+
+class HttpError extends BaseError {};
+
+const errorTypes = [
+    {
+        name: 'BadRequestError',
+        code: 400,
+    },
+    {
+        name: 'UnauthorizedError',
+        code: 401,
+    },
+    {
+        name: 'ForbiddenError',
+        code: 403,
+    },
+    {
+        name: 'NotFoundError',
+        code: 404,
+    },
+    {
+        name: 'NotAcceptableError',
+        code: 406,
+    },
+    {
+        name: 'ProxyAuthenticationRequiredError',
+        code: 407,
+    },
+    {
+        name: 'RequestTimeoutError',
+        code: 408,
+    },
+    {
+        name: 'ConflictError',
+        code: 409,
+    },
+    {
+        name: 'GoneError',
+        code: 410,
+    },
+    {
+        name: 'LengthRequiredError',
+        code: 411,
+    },
+    {
+        name: 'PreconditionFailedError',
+        code: 412,
+    },
+    {
+        name: 'RequestEntityTooLargeError',
+        code: 413,
+    },
+    {
+        name: 'RequestURITooLongWError',
+        code: 414,
+    }
+];
+
+module.exports = (() => {
+    const errors = {};
+    errorTypes.forEach(error => {
+        errors[error.name] = generator(error, HttpError);
+    });
+    return {
+        ...errors,
+        HttpError,
+    };
+})();
