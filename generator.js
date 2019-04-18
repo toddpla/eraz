76709316a533fb9ef9beb7d734d1c3c72ca9a88e
Todd Langford-Archer <toddpla@gmail.com>
2019-04-18 07:49:29 +0100

    Refactor generator with better validation

--- generator.js
+++ generator.js
@@ -1,12 +1,15 @@
 const BaseError = require('./BaseError');
 
-module.exports = (classAttributes, ParentError = BaseError) => {
+module.exports = (classAtts, ParentError = BaseError) => {
     if (!(ParentError.prototype instanceof BaseError)) throw new Error('Parent does not extend from BaseError');
+    if (!classAtts.name === undefined) throw new Error('Class attributes must include a name for the eror i.e. (NotFoundError)')
     return class extends ParentError {
-        constructor(message, instanceAttributes) {
+        constructor(message, initArgs) {
             super(message);
-            Object.assign(this, classAttributes);
-            Object.assign(this, instanceAttributes);
+            if (!typeof(classAtts) === 'object') throw new Error('Class attributes are not an object for class');
+            Object.assign(this, classAtts);
+            if (initArgs !== undefined && !typeof(initArgs) === 'object') throw new Error('Class attributes passed is not an object.');
+            Object.assign(this, initArgs);
         }
     };
 };
